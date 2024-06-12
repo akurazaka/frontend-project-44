224fccb99d1f31c798848d038a6315032824dc00
Your Name <you@example.com>
2024-06-12 20:55:13 +0600

    Create new game calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+
+const description = 'What is the result of the expression?';
+
+const generateRandomNumber = () => Math.floor(Math.random() * 100);
+
+const operations = ['+', '-', '*'];
+
+const calculate = (a, operator, b) => {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    default:
+      throw new Error(`Unknown operator: '${operator}'`);
+  }
+};
+
+const generateRound = () => {
+  const num1 = generateRandomNumber();
+  const num2 = generateRandomNumber();
+  const operator = operations[Math.floor(Math.random() * operations.length)];
+  const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = String(calculate(num1, operator, num2));
+  return [question, correctAnswer];
+};
+
+const playCalcGame = () => playGame(description, generateRound);
+
+playCalcGame();
---
+++ bin/brain-even.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const generateRandomNumber = () => Math.floor(Math.random() * 100);
+
+const isEven = (num) => num % 2 === 0;
+
+const generateRound = () => {
+  const randomNumber = generateRandomNumber();
+  const question = `${randomNumber}`;
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const playEvenGame = () => playGame(description, generateRound);
+
+playEvenGame();
