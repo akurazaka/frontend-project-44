cef07765327f024c4faae3cd6bc43904645c0d89
Your Name <you@example.com>
2024-06-12 21:02:08 +0600

    Create new game gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,23 @@
+const gcd = (a, b) => {
+    if (!b) {
+      return a;
+    }
+    return gcd(b, a % b);
+  };
+  
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateRound = () => {
+    const num1 = generateRandomNumber(1, 100);
+    const num2 = generateRandomNumber(1, 100);
+    const question = `${num1} ${num2}`;
+    const correctAnswer = String(gcd(num1, num2));
+    return [question, correctAnswer];
+};
+
+const playGcdGame = () => playGame(description, generateRound);
+
+playGcdGame();
